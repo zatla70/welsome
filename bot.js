581ba349262abb99b0b5463e4ffaf9a5288b985c
zatla70 <44783807+zatla70@users.noreply.github.com>
2018-12-04 15:50:26 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,13 +38,13 @@ client.on('guildMemberAdd',async member => {
             ctx.fontSize = '72px';
             ctx.fillStyle = "#ffffff";
             ctx.textAlign = "center";
-            ctx.fillText(member.user.username, 410, 174);
+            ctx.fillText(member.user.username, 400, 170);
            
             ctx.font = '18px Arial Bold';
             ctx.fontSize = '72px';
             ctx.fillStyle = "#ffffff";
             ctx.textAlign = "center";
-            ctx.fillText(`${member.guild.memberCount} Members`, 505, 195);
+            ctx.fillText(`${member.guild.memberCount} Members`, 500, 195);
            
             let Avatar = Canvas.Image;
             let ava = new Avatar;
