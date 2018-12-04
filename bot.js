cada93fe349db03157d60bb7374f4569ac3b680f
zatla70 <44783807+zatla70@users.noreply.github.com>
2018-12-04 16:47:34 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ client.on('guildMemberAdd',async member => {
             ctx.fontSize = '80px';
             ctx.fillStyle = "#ffffff";
             ctx.textAlign = "center";
-            ctx.fillText(member.user.username, 445, 207);
+            ctx.fillText(member.user.username, 455, 207);
            
             let Avatar = Canvas.Image;
             let ava = new Avatar;
@@ -52,10 +52,81 @@ client.on('guildMemberAdd',async member => {
             let c = member.guild.channels.find('name', `welcome`)
             if(!c) return;
             c.sendFile(canvas.toBuffer());
+            c.sendFile(canvas.toBuffer())
+      setTimeout(async function(){
+        c.send('**  ̲W̲e̲l̲c̲o̲m̲e   ̲t̲o  ̲Server BetaShop  :two_hearts: :sparkling_heart:  :sparkles: **')
+      }, 2000);
    
   });
   });
   });
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+ guild = client.guilds.get("517459006613094400")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+client.on("guildMemberAdd", (member) => {
+let channel = client.channels.get("519145834714103808");
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("517459006613094400")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+                  setTimeout(async function(){
+        channel.send('**Invite By ${Invite.inviter}**')
+      }, 2000);          
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+});
+
+
+
+client.on("guildMemberAdd", member => {
+  const moment = require('moment');
+let welcomer = member.guild.channels.find("name","welcome");
+      if(!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+         var h = member.user;
+        let norelden = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(h.avatarURL)
+        .setAuthor(h.username,h.avatarURL)
+        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
+         .addField(': تاريخ دخولك السيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true)      
+         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+        setTimeout(async function(){
+        welcomer.send({embed:norelden});          
+               }, 2100);  
+ 
+      }
+      });
+
 
 
 
