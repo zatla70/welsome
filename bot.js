595072cd95d120b4691fa2f2fd84790dc3566be5
zatla70 <44783807+zatla70@users.noreply.github.com>
2018-12-04 16:51:22 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -120,9 +120,8 @@ let welcomer = member.guild.channels.find("name","welcome");
         .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
          .addField(': تاريخ دخولك السيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true)      
          .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
-        setTimeout(async function(){
         welcomer.send({embed:norelden});          
-               }, 2100);  
+  
  
       }
       });
